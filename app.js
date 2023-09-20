8e8f278e2c8c8f5b6cfecff61e5aa8aa2d8afa93
Konstantin <Konstantin.schepkin@gmail.com>
2023-09-20 19:53:30 +0300

    Init

---
+++ app.js
@@ -0,0 +1,147 @@
+document.getElementById("addMember").addEventListener("click", addMemberRow);
+document.getElementById("saveData").addEventListener("click", saveData);
+document.getElementById("fileInput").addEventListener("change", loadData);
+document.getElementById("startDate").addEventListener("change", updateWorkDays);
+document.getElementById("endDate").addEventListener("change", updateWorkDays);
+
+function addMemberRow() {
+    const tableBody = document.getElementById("teamTable").getElementsByTagName("tbody")[0];
+    const newRow = tableBody.insertRow();
+
+    const nameCell = newRow.insertCell(0);
+    const workDaysCell = newRow.insertCell(1);
+    const workHoursCell = newRow.insertCell(2);
+    const extraHolidaysCell = newRow.insertCell(3);
+    const otherActivitiesCell = newRow.insertCell(4);
+    const capacityCell = newRow.insertCell(5);
+    const actionCell = newRow.insertCell(6);
+
+    nameCell.innerHTML = '<input type="text" class="form-control">';
+    workDaysCell.innerHTML = '<input type="number" class="form-control" value="0">';
+    workHoursCell.innerHTML = '<input type="number" class="form-control" value="8">';
+    extraHolidaysCell.innerHTML = '<input type="number" class="form-control" value="0">';
+    otherActivitiesCell.innerHTML = '<input type="number" class="form-control" value="0">';
+    capacityCell.innerHTML = '<span>0</span>';
+    actionCell.innerHTML = '<button class="btn btn-danger">Удалить</button>';
+
+    actionCell.querySelector("button").addEventListener("click", function() {
+        tableBody.removeChild(newRow);
+        calculateTotalCapacity();
+    });
+
+    // Add event listeners to inputs for capacity calculations
+    workDaysCell.querySelector("input").addEventListener("input", calculateCapacity);
+    workHoursCell.querySelector("input").addEventListener("input", calculateCapacity);
+    extraHolidaysCell.querySelector("input").addEventListener("input", calculateCapacity);
+    otherActivitiesCell.querySelector("input").addEventListener("input", calculateCapacity);
+
+    // Установим рабочие дни для нового сотрудника на основе текущих дат
+    updateWorkDaysForNewMember(workDaysCell);
+}
+
+function calculateCapacity(event) {
+    const row = event.target.closest("tr");
+    const workDays = parseInt(row.cells[1].querySelector("input").value);
+    const workHours = parseInt(row.cells[2].querySelector("input").value);
+    const extraHolidays = parseInt(row.cells[3].querySelector("input").value);
+    const otherActivitiesDays = parseInt(row.cells[4].querySelector("input").value);
+
+    const capacity = (workDays * workHours) - (extraHolidays * workHours) - (otherActivitiesDays * workHours);
+    row.cells[5].querySelector("span").innerText = capacity;
+
+    calculateTotalCapacity();
+}
+
+
+function calculateTotalCapacity() {
+    const tableBody = document.getElementById("teamTable").getElementsByTagName("tbody")[0];
+    let totalCapacity = 0;
+
+    for (let row of tableBody.rows) {
+        const capacity = parseInt(row.cells[5].querySelector("span").innerText);
+        totalCapacity += capacity;
+    }
+
+    document.getElementById("totalCapacity").innerText = "Общий capacity команды: " + totalCapacity;
+}
+
+function updateWorkDaysForNewMember(workDaysCell) {
+    const startDate = new Date(document.getElementById("startDate").value);
+    const endDate = new Date(document.getElementById("endDate").value);
+    let workDays = 0;
+
+    for (let day = startDate; day <= endDate; day.setDate(day.getDate() + 1)) {
+        if (day.getDay() !== 0 && day.getDay() !== 6) {  // Exclude Sundays (0) and Saturdays (6)
+            workDays++;
+        }
+    }
+    
+    workDaysCell.querySelector("input").value = workDays;
+}
+
+function updateWorkDays() {
+    const tableBody = document.getElementById("teamTable").getElementsByTagName("tbody")[0];
+    const startDate = new Date(document.getElementById("startDate").value);
+    const endDate = new Date(document.getElementById("endDate").value);
+    let workDays = 0;
+
+    for (let day = startDate; day <= endDate; day.setDate(day.getDate() + 1)) {
+        if (day.getDay() !== 0 && day.getDay() !== 6) {  // Exclude Sundays (0) and Saturdays (6)
+            workDays++;
+        }
+    }
+
+    for (let row of tableBody.rows) {
+        row.cells[1].querySelector("input").value = workDays;
+        calculateCapacity({ target: row.cells[2].querySelector("input") });
+    }
+}
+
+function saveData() {
+    const tableBody = document.getElementById("teamTable").getElementsByTagName("tbody")[0];
+    const data = [];
+
+    for (let row of tableBody.rows) {
+        const memberData = {
+            name: row.cells[0].querySelector("input").value,
+            workHours: row.cells[2].querySelector("input").value,
+            extraHolidays: row.cells[3].querySelector("input").value,
+            otherActivities: row.cells[4].querySelector("input").value
+        };
+        data.push(memberData);
+    }
+
+    const blob = new Blob([JSON.stringify(data)], { type: "application/json" });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "team_data.json";
+    a.click();
+}
+
+function loadData(event) {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.readAsText(file);
+        reader.onload = function() {
+            const data = JSON.parse(reader.result);
+            const tableBody = document.getElementById("teamTable").getElementsByTagName("tbody")[0];
+            while (tableBody.rows.length) {
+                tableBody.deleteRow(0);
+            }
+            for (let member of data) {
+                addMemberRow();
+                const newRow = tableBody.rows[tableBody.rows.length - 1];
+                newRow.cells[0].querySelector("input").value = member.name;
+                newRow.cells[2].querySelector("input").value = member.workHours;
+                newRow.cells[3].querySelector("input").value = member.extraHolidays;
+                newRow.cells[4].querySelector("input").value = member.otherActivities;
+                calculateCapacity({ target: newRow.cells[2].querySelector("input") });
+            }
+        };
+    }
+}
+
+// Для демонстрации добавим 1 строку при загрузке страницы
+window.onload = addMemberRow;
